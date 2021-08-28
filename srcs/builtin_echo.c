4e96cb0b2efd6109fc39012f621a1181699cdd13
saoh <saoh@student.42seoul.kr>
2021-08-28 16:22:19 +0900

    modify echo

--- srcs/builtin_echo.c
+++ srcs/builtin_echo.c
@@ -8,13 +8,13 @@ void	builtin_echo(t_info *info)
 
 	i = 1;
 	n_flag = 0;
-	if (info->param[1][0] == '-')
+	argc = get_argc(info->param);
+	if (argc > 1 && info->param[1][0] == '-')
 	{
 		if (info->param[1][1] == 'n')
 			n_flag++;
 		i++;
 	}
-	argc = get_argc(info->param);
 	while (i < argc)
 	{
 		if (i == argc - 1)
