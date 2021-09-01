63d411174f538816cb01253d386873d21065235e
taesan <pawer02023@gmail.com>
2021-09-01 15:52:18 +0900

    header modify & perror modify

--- includes/defines.h
+++ includes/defines.h
@@ -6,7 +6,7 @@
 /*   By: taesan <taesan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 15:05:24 by taesan            #+#    #+#             */
-/*   Updated: 2021/08/30 16:32:43 by saoh             ###   ########.fr       */
+/*   Updated: 2021/09/01 15:38:04 by taesan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,26 +43,23 @@ typedef	struct	s_data_to_temp
 	char	*key;
 	char	*param;
 }				t_data_to_temp;
-/*
-	명령어에서 아래 문자들이 존재하는지 ..
-	single, double모두 존재할 경우, 1번이 먼저 나옴.
-*/
+
 typedef struct s_info
 {
-	t_list	*commands; // 마지막에 clear
-	t_list	*commands_symbol; // 마지막에 clear
-	t_list	*redirect_lst; // exec하고 clear
+	t_list	*commands;
+	t_list	*commands_symbol;
+	t_list	*redirect_lst;
 	t_list	*param_list;
 	t_list	*temp_list;
 	void	(*sigint)(int);
 	void	(*sigquit)(int);
-	char	**paths; // 계속 씀.
-	char	**param; // parents에서 free됨.
-	char	**envp; // 계속 씀.
+	char	**paths;
+	char	**param;
+	char	**envp;
 	char	*input;
-	int		command_cnt; // 유동적.
-	int		asterisk_check; // 유동적
-	int		is_builtin; // param만들면서 체크 함.
+	int		command_cnt;
+	int		asterisk_check;
+	int		is_builtin;
 	int		envp_cnt;
 	int		param_cnt;
 	int		pipe_in[2];
@@ -70,9 +67,11 @@ typedef struct s_info
 	int		connect_pipe[2];
 	int		redirect_fd[2];
 	int		std_in;
-	int		exec_result; // exec하고 init
+	int		exec_result;
 }				t_info;
-
+/*
+	constants
+*/
 # define INPUT_CHANGED 1
 # define NOT_BUILTIN 0
 # define ECHO 1
@@ -92,21 +91,6 @@ typedef struct s_info
 # define DB_PIPE 2
 # define DB_AMPER 3
 # define EXEC_FAIL 42
-
-
-# define PATH "PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
-# define SPLIT_ERR "ft_split Error"
-# define MALLOC_ERR "malloc Error"
-# define WAIT_ERR "wait func return Error [-1]"
-# define SUBSTR_ERR "ft_substr Error"
-# define STRJOIN_ERR "ft_strjoin Error"
-# define MAKE_NEW_INPUT_ERR "make new input Error"
-# define FILTER_INPUT_ERR "filter_input error"
-# define NOT_CLOSED "quotes are not closed"
-/*
-	pipex defines ...ref..
-*/
-
 # define READ_FD_IDX 0
 # define WRITE_FD_IDX 1
 # define STDIN_PIPE 0x1
@@ -114,29 +98,37 @@ typedef struct s_info
 # define BUFFER_SIZE 255
 # define FD_MAX 255
 # define PATH "PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+/*
+	program Error
+*/
+# define SPLIT_ERR "ft_split Error"
+# define MALLOC_ERR "malloc Error"
+# define WAIT_ERR "wait func return Error [-1]"
+# define MAKE_NEW_INPUT_ERR "make new input Error"
+# define FILTER_INPUT_ERR "filter_input error"
+# define NOT_CLOSED "quotes are not closed"
 # define PARSE_ERR "PARSING ERROR"
 # define SPLIT_ERR "ft_split Error"
 # define INPUT_OPEN_ERR "input file open"
-
-/* bash Error */
+# define FILE_OPEN_ERR "file open Error"
+# define DUP2_ERR "dup2"
+# define PIPE_ERR "create pipe Error"
+# define UNLINK_ERR "file remove Error"
+/* 
+	bash Error 
+*/
 # define NO_SUCH_FILE "No such file or directory"
 # define BAD_FD "Bad file descriptor"
 # define UNEXPECTED_TOKEN "syntax error near unexpected token `&\'"
-
-# define FILE_OPEN_ERR "file open Error"
-# define OUTPUT_OPEN_ERR "redirection output file open"
-# define COMMAND_NOT_EXIST "command program not exist"
-# define ENV_PATH_NOT_EXIST "system env PATH not exist"
-# define DUP2_ERR "dup2"
-# define ENV_PATH_NOT_EXIST "system env PATH not exist"
-# define PIPE_ERR "create pipe Error"
-# define READ_ERR "read result [-1]"
-# define UNLINK_ERR "file remove Error"
+# define EXIT_NUMERIC_ERR "exit: -n: numeric argument required"
+# define EXIT_MANY_ARGS "exit \nexit: too many arguments"
+/*
+	file names
+*/
 # define TEMP_FILE ".temp.txt"
 # define EXPORT_FILE ".export.txt"
 # define EXPORT_FILE_2 ".export_2.txt"
 # define ENV_FILE ".env.txt"
 # define ENV_FILE_2 ".env_2.txt"
-
 
 #endif
--- includes/minishell.h
+++ includes/minishell.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taesan <taesan@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/01 15:50:59 by taesan            #+#    #+#             */
+/*   Updated: 2021/09/01 15:50:59 by taesan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
@@ -15,7 +27,6 @@
 # include <string.h>
 # include "../libft/libft.h"
 # include "defines.h"
-
 /*
 	error.c
 */
@@ -31,8 +42,8 @@ int		is_empty(char *temp);
 int		is_redirect(char c);
 int		is_double_symbol(int symbol);
 void	ft_close(int fd);
-
 void	jump_space(char *input, int *i);
+void	clear_pipe(t_info *info, int pipe[2], int flags);
 /*
 	check
 */
@@ -40,16 +51,14 @@ int		check_builtin(char *cmd);
 char	*check_command(char **paths, char *cmd, int len);
 int		exist_dollar(char *input, int s, int e);
 /*
-	readline
-*/
-void	wait_closing_quote(char start_q);
-/*
 	init
 */
-int		init_info(t_info *info);
 int		init_command_info(t_info *info, char *input);
 int		init_envp_and_signal(t_info *info, char *envp[]);
 int		copy_envp(t_info *info);
+/*
+	sig_handler
+*/
 void	ctrl_d_handler(void);
 void	sigint_handler(int signo);
 /*
@@ -59,34 +68,32 @@ void	split_free(char **data);
 void	ft_free(void *data);
 void	clear_all_data(t_info *info);
 void	clear_data(t_info *info);
-void	content_not_rm(void *param);
 /*
 	exec
 */
-void		exec_call(t_info *info, int instruct_seq);
+void	exec_call(t_info *info, int instruct_seq);
 /*
-	make command list
+	make_command_list
 */
-int	make_command_list(t_info *info, char *input);
-int	append_command(t_info *info, char *input, int s, int e);
-int	append_pipe_command(t_info *info, char *input, int *s, int *e);
-int	append_amper_command(t_info *info, char *input, int *s, int *e);
+int		make_command_list(t_info *info, char *input);
+int		append_command(t_info *info, char *input, int s, int e);
+int		append_pipe_command(t_info *info, char *input, int *s, int *e);
+int		append_amper_command(t_info *info, char *input, int *s, int *e);
 /*
 	filter
 */
-int	command_filter(t_info *info, char **content);
-int	redirect_filter(t_info *info, char **content);
-int	redirect_get_end(char *content, int *e);
-int	move_redirect_space(char *content, int *e, int idx);
-int	move_redirect_and(char *content, int *e, int idx);
+int		command_filter(t_info *info, char **content);
+int		redirect_filter(t_info *info, char **content);
+int		redirect_get_end(char *content, int *e);
+int		move_redirect_space(char *content, int *e, int idx);
+int		move_redirect_and(char *content, int *e, int idx);
 void	move_quot_point(char *line, int *e, char end_c);
 /*
 	replace_env
 */
 int		replace_env(char **envp, char **ptr, int s, int *next_idx);
-
 /*
-	redirect util
+	redirect_util
 */
 int		remove_redirect(int s, int e, char **content);
 int		get_ampersand_fd(char *content, int i, int fds[2]);
@@ -99,54 +106,35 @@ int		get_next_line(int fd, char **line);
 char	*content_null(char *buf);
 char	*check_first(int fd, char **line);
 /*
-	test. 지워야 함
-*/
-void	command_to_string(t_info info);
-/*
-	sig_handler
-*/
-void	sigquit_handler(int signo);
-void	sigint_handler(int signo);
-/*
 	set_connect_pipe
 */
 int		set_connect_pipe(t_info *info, int seq);
 /*
-	exec_redirection
-*/
-int		redirection_dup(t_info *info);
-
-/*
-	redirect_in_dup
+	redirect_dup(int & out)
 */
 int		redirect_in_dup(t_info *info, int std_in, char *content);
 int		redirect_out_dup(int fds[2], char *content);
+int		redirection_dup(t_info *info);
 /*
 	exec_builtin
 */
 void	exec_builtin(int cmd, t_info *info);
-void	clear_pipe(t_info *info, int pipe[2], int flags);
-
 /*
 	sort
 */
 void	merge_sort(char **sorted, char **temp, int idx_l, int idx_r);
-
 /*
 	make param
 */
-int	make_param(t_info *info, char *input, int len);
-
+int		make_param(t_info *info, char *input, int len);
 /*
 	write_env_file
 */
-int	write_env_file(char *key, char *param);
-int remove_var(char *r_file, char *w_file, char *key);
-
+int		write_env_file(char *key, char *param);
+int		remove_var(char *r_file, char *w_file, char *key);
 /*
-	write_export_file
+	builtin
 */
-int		write_export_file(char *var);
 void	builtin_exit(t_info *info);
 void	builtin_echo(t_info *info);
 void	builtin_cd_sib(t_info *info);
@@ -156,51 +144,31 @@ void	builtin_env(t_info *info);
 void	builtin_unset(t_info *info);
 void	builtin_export(t_info *info);
 void	builtin_set(t_info *info, int pipe[2]);
-
+int		get_argc(char **argv);
 /*
-	export_print.c
+	export
 */
+int		write_export_file(char *var);
 int		print_export(t_info *info);
 void	export_errror(char *param);
-/*
-	export_file_merge
-*/
 int		export_file_merge(t_info *info, t_list **list);
-/*
-	export_add_var
-*/
 void	export_add_var(t_info *info);
-/*
-	정리하자
-*/
 int		env_file_cmp(const char *line, const char *key);
-/*
-	1 -> 2
-	2 -> 1  함수
-*/
-int		temp_to_datafile(char *read, char *write); // 공통인데 어따 뺄까이거..
+int		temp_to_datafile(char *read, char *write);
 int		datafile_to_temp(char *r_file, char *w_file, char *key, char *param);
 /*
 	filter_asterisk
 */
 int		filter_asterisk(t_info *info, int i);
-
 /*
 	list_to_string
 */
 t_list	*list_to_string(t_info *info, char *input, int *i, int *s);
 t_list	*front_back_join(char *input, t_strjoin *temp);
-
 /*
 	free_utils
 */
 void	remove_file(void);
-
-/*
-	test
-*/
-int		print_export(t_info *info);
-int		get_argc(char **argv);
-
+void	content_not_rm(void *param);
 
 #endif
